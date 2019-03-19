f748d0f2787bad48e9ea463cd980fdd5beee20a0
Nolan Lapham <nlapham21@gmail.com>
2019-03-19 08:17:55 -0400

    setup production build and server

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.jsx',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js?x$/,
-            exclude: /node_modules/,
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader',
-            ],
-        }],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true, // Client side routing
-    },
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        mode: isProduction ? 'production' : 'development',
+        entry: './src/app.jsx',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js',
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js?x$/,
+                exclude: /node_modules/,
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            }],
+        },
+        resolve: {
+            extensions: ['.js', '.jsx'],
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true, // Client side routing
+        },
+    };
 };
